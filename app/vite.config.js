5c883061a9af54ea2b6980a577036750d29df2bf
sieeeerra <usesigmchi@gmail.com>
2025-11-17 14:26:41 +0900

    Adjust Vite configuration to set chunk size warning limit to 1000KB for improved build management

--- app/vite.config.js
+++ app/vite.config.js
@@ -11,5 +11,9 @@ export default defineConfig({
         api: 'modern-compiler' // modern API 사용
       }
     }
+  },
+  // 빌드 시 청크 크기 경고 제한 조정
+  build: {
+    chunkSizeWarningLimit: 1000 // 1000KB (1MB)로 경고 임계값 설정
   }
 });
